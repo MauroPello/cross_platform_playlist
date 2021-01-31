6e56127f47092c33dafa0fd536d03a727e6f17e7
MauroPello <mauro.pellonara@outlook.com>
2021-01-31 23:11:50 +0100

    Added indexes

--- php/view_playlist.php
+++ php/view_playlist.php
@@ -11,15 +11,16 @@
         <table class="table table-sm table-hover table-dark" style="text-align: left; background-color: #272727;">
             <thead>
                 <tr>
+                <th scope="col"><b>#</b></th>
                 <th scope="col"><b>Name (click any to start playing)</b></th>
                 <th scope="col"><b>Direct Link</b></th>
                 </tr>
             </thead>
             <tbody>
                 <?php 
-                    foreach($songs as $song){
-                        $tmp = explode("*|*", $song);
-                        echo '<tr><th scope="row"><button type="submit" name="button" value="Play_Song*|*' . $tmp[1] . '" class="btn btn-link" style="margin: 0; padding: 0; text-align: left;">' . $tmp[0] . '</button><td><a href="' . get_url($tmp[1], $tmp[2]) . '" target="_blank">' . get_url($tmp[1], $tmp[2]) . '</a></td></th></tr>';
+                    for($i = 0; $i < count($songs); $i++){
+                        $tmp = explode("*|*", $songs[$i]);
+                        echo '<tr><td>' . $i . '</td><td><button type="submit" name="button" value="Play_Song*|*' . $tmp[1] . '" class="btn btn-link" style="margin: 0; padding: 0; text-align: left;">' . $tmp[0] . '</button></td><td><a href="' . get_url($tmp[1], $tmp[2]) . '" target="_blank">' . get_url($tmp[1], $tmp[2]) . '</a></td></tr>';
                     }
                 ?>
             </tbody>
