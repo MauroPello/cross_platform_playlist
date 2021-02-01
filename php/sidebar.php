23960048b05bf397285074bf09b83201e59c258e
MauroPello <mauro.pellonara@outlook.com>
2021-02-01 15:54:08 +0100

    Updated TODO

--- php/sidebar.php
+++ php/sidebar.php
@@ -7,12 +7,9 @@
             <a class="nav-item" href="./" style="margin-bottom: 15%; font-weight: bold; width: 80%; font-size: 1.25rem; color: white;">All Around Playlist</a>
 
             <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" style="width: 80%;">
-                <span class="nav-item" style="margin-bottom: 5%; font-weight: bold; width: 80%;">
-                    <?php echo "User: " . $_COOKIE["username"]; ?>
-                </span>
-                <button class="btn btn-link nav-item" type="submit" name="button" value="Log_Out" style="margin-bottom: 5%; font-weight: bold; width: 80%;">
-                    Log Out
-                </button>
+                <span class="nav-item" style="margin-bottom: 5%; font-weight: bold; width: 80%;"><?php echo "User: " . $_COOKIE["username"]; ?></span>
+                <button class="btn btn-link nav-item" type="submit" name="button" value="Log_Out" style="margin-bottom: 5%; font-weight: bold; width: 80%;">Log Out</button>
+                
                 <div class="form-group">
                     <div class="input-group mb-3">
                         <input style="border-radius: .25rem!important;" type="text" name="playlist" list="playlists" class="form-control" value="<?php echo $playlist; ?>" placeholder="Playlist" autocomplete="off" aria-label="Playlist" aria-describedby="inputGroup-sizing-default">
