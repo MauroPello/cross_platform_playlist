4d005314ef7870a2b942b7984e211efd6eed4197
MauroPello <mauro.pellonara@outlook.com>
2021-02-07 22:24:33 +0100

    Changed Site Title

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ update_session_activity();
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/sign-in/">
     <link rel="icon" href="img/logo.ico">
-    <title>All Around Playlists</title>
+    <title>All Around Playlist</title>
 </head>
 
 <body class="text-center">
